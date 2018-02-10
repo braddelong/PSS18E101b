fabab060c39f6ab6a203f0f5b80be5060a03fc35
Brad DeLong <brad.delong@gmail.com>
2018-02-09 16:53:59 -0800

    Initial commits...
    Making sure PS files are where they should be...

---
+++ ok_tests/q09.py
@@ -0,0 +1,54 @@
+test = {
+  'name': '',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+               {
+          'code': r"""
+          >>> import numpy as np
+          >>> np.isclose(YoverLfourone, 5)
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        },
+        
+               {
+          'code': r"""
+          >>> import numpy as np
+          >>> np.isclose(YKverLfourone, 25)
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        },
+      
+               {
+          'code': r"""
+          >>> import numpy as np
+          >>> np.isclose(YoverLfourtwo, 4.5
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        },
+    
+               {
+          'code': r"""
+          >>> import numpy as np
+          >>> np.isclose(KoverLfourtwo, 22.4)
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+     
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
---
+++ ok_tests/q12.py
@@ -0,0 +1,54 @@
+test = {
+  'name': '',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+               {
+          'code': r"""
+          >>> import numpy as np
+          >>> np.isclose(YoverL47, 1900)
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        },
+        
+               {
+          'code': r"""
+          >>> import numpy as np
+          >>> np.isclose(KoverL47, 4800)
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        },
+      
+               {
+          'code': r"""
+          >>> import numpy as np
+          >>> np.isclose(YoverL48, 36)
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        },
+    
+               {
+          'code': r"""
+          >>> import numpy as np
+          >>> np.isclose(KoverL48, 151)
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+    
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
