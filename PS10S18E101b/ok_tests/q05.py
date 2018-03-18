20d4e01cc2471de57a6791a8a9722e16dc96e240
Brad DeLong <brad.delong@gmail.com>
2018-03-18 12:05:47 -0700

    PS 9
    getting close...

--- PS10S18E101b/ok_tests/q05.py
+++ PS10S18E101b/ok_tests/q05.py
@@ -6,15 +6,23 @@ test = {
       'cases': [
         {
           'code': r"""
-          >>> np.isclose(GAP1, 0, atol=0.1) 
+          >>> np.isclose(c_y_1_quarter, 0.6036, atol=0.1) 
           True
           """,
           'hidden': False,
           'locked': False
         },
-         {
+        {
           'code': r"""
-          >>> at_equilibrium
+          >>> np.isclose(c_y_1_quarter_early_sample, 0.5484, atol=0.1) 
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        },
+        {
+          'code': r"""
+          >>> np.isclose(c_y_1_quarter_late_sample, 0.7408, atol=0.1) 
           True
           """,
           'hidden': False,
