e46766839bc0a5750068df06241c56e82066c934
Brad DeLong <brad.delong@gmail.com>
2018-04-19 10:40:23 -0700

    cleaning up PS11
    cleaning up PS11

---
+++ PS11S18E101b_back/ok_tests/q01.py
@@ -0,0 +1,77 @@
+test = {
+  'name': '',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> import numpy as np
+          >>> np.isclose(Delta_Y_PS11taskB, 0.2, atol=0.01)
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        },
+        {
+          'code': r"""
+          >>> import numpy as np
+          >>> np.isclose(Delta_r_PS11taskB, 0, atol=0.01)
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        },
+        {
+          'code': r"""
+          >>> import numpy as np
+          >>> np.isclose(Delta_C_PS11taskB, 0.1, atol=0.01)
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        },
+        {
+          'code': r"""
+          >>> import numpy as np
+          >>> np.isclose(Delta_I_PS11taskB, 0, atol=0.01)
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        },
+        {
+          'code': r"""
+          >>> import numpy as np
+          >>> np.isclose(Delta_G_PS11taskB, 0, atol=0.01)
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        },
+        {
+          'code': r"""
+          >>> import numpy as np
+          >>> np.isclose(Delta_NX_PS11taskB, 0.1, atol=0.01)
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        },
+        {
+          'code': r"""
+          >>> import numpy as np
+          >>> np.isclose(Delta_epsilon_PS11taskB, 0.1, atol=0.01)
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
